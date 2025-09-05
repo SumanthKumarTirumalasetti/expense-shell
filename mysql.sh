c8088ad34b11d62787b430dfc09afcd625a8977e
Sumanth Tirumalasetti <st185178@candescent.com>
2025-09-05 21:24:18 +0530

    Building mysql server

--- mysql.sh
+++ mysql.sh
@@ -46,7 +46,7 @@ validate $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 validate $? "Starting MySQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 -e 'show databases'
+mysql_secure_installation --set-root-pass ExpenseApp@1 -e 'show databases;'
 
 if [ $? -ne 0 ]
 then
