c2d561f97d06b513ca18b064a8496827fd7a7285
Sumanth Tirumalasetti <st185178@candescent.com>
2025-09-06 13:52:42 +0530

    Building frontend server

--- frontend.sh
+++ frontend.sh
@@ -41,30 +41,30 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
-dnf install nginx -y 
+dnf install nginx -y &>>$LOG_FILE_NAME
 validate $? "Installing nginx server"
 
-systemctl enable nginx
+systemctl enable nginx &>>$LOG_FILE_NAME
 validate $? "enable nginx server"
 
-systemctl start nginx
+systemctl start nginx &>>$LOG_FILE_NAME
 validate $? "Starting nginx server"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAME
 validate $? "Removing existing version of codes"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE_NAME
 validate $? "Downloading latest code in /tmp"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>$LOG_FILE_NAME
 validate $? "Moving to HTML directory"
 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
 validate $? "Unzipping frontend code"
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOG_FILE_NAME
 validate $? "Copying expense.conf to default.d directory"
 
-systemctl restart nginx
+systemctl restart nginx &>>$LOG_FILE_NAME
 validate $? "Restarting nginx server"
 
