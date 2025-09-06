33ea805cf4da4e280b435f67882ac7e93a0108e3
Sumanth Tirumalasetti <st185178@candescent.com>
2025-09-06 13:28:45 +0530

    Building backend server

--- backend.sh
+++ backend.sh
@@ -89,7 +89,7 @@ validate $? "Setting up the transactions schema and tables"
 systemctl daemon-reload &>>$LOG_FILE_NAME
 validate $? "daemon-reload"
 
-systemctl restart backned &>>$LOG_FILE_NAME
+systemctl restart backend &>>$LOG_FILE_NAME
 validate $? "Restarting backend"
 
 systemctl enable backend &>>$LOG_FILE_NAME
